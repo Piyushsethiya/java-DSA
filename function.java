e78f9d985644a31249c0be6e80c19d5c7e3d008d
Piyush Sethiya <piyushseth226@gmail.com>
2024-07-16 16:30:47 +0530

    odd number half

---
+++ function.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class function {
+
+  public static void printname(String name) {
+    System.out.println("My name is " + name);
+    return;
+  }
+
+  // Make a multiply and return there product
+
+  public static int multiply(int a, int b) {
+    int product = a * b;
+    return product;
+  }
+
+  // find factorial of 2 no.
+  public static int factorial(int n) {
+    int fact = 1;
+    if (n == 0) {
+      return 1;
+    } else {
+      for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+      }
+      return fact;
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // String name = sc.next();
+    // printname(name);
+
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // System.out.println("Total Number: " + multiply(a, b));
+
+    int n = sc.nextInt();
+    System.out.println("Total Number of factorial: " + factorial(n));
+  }
+}
