d459bff2dcb64284d6e9273ba5fd38af0085b33f
Piyush Sethiya <piyushseth226@gmail.com>
2024-07-23 15:12:06 +0530

    ll start

---
+++ linked_list.java
@@ -0,0 +1,19 @@
+public class linked_list {
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+// add first, last
+
+
+    public static void main(String[] args) {
+        linked_list list = new linked_list();
+
+    }
+}
--- stack.java
+++ stack.java
@@ -1,3 +1,29 @@
+import java.lang.classfile.components.ClassPrinter.Node;
+
 public class stack {
-    
+    static class node {
+        int data;   
+        Node next;
+        public node(int data){
+            this.data = data;
+            next = null;
+        }
+        
+    }
+    static class Stack {
+        public static Node head;
+        public static boolean isEmpty() {
+            return head == null;
+        }
+        public static void push(int data) {
+            // Node newNode = new node(data);
+            if(isEmpty()){
+                // head = newNode;
+                return;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
 }
