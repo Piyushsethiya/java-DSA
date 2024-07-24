f39d6d6bbe1573945177822479c0802d05b7d053
Piyush Sethiya <piyushseth226@gmail.com>
2024-07-24 14:41:17 +0530

    ll half

--- linked_list.java
+++ linked_list.java
@@ -1,19 +1,61 @@
 public class linked_list {
-    class Node{
+    class Node {
         String data;
         Node next;
 
-        Node(String data){
+        Node(String data) {
             this.data = data;
             this.next = null;
         }
     }
 
-// add first, last
-
+    // add first, last
+Node head;
+    public void addFirst(String data) {
+        
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public void add(Node prev , String data) {
+        
+        Node newNode = new Node(data);
+        System.out.println(prev);
+        if (prev == null) {
+            head = newNode;
+            return;
+        }
+        prev.next= newNode;
+    }
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while( currNode.next != null){
+            currNode = currNode.next;
+        }
+    }
 
     public static void main(String[] args) {
         linked_list list = new linked_list();
+        Node currNode = list.head;
+        list.add(null,"a");
+        list.add( currNode,"is");
+        list.addLast("last");
+
+        Node copyHead = list.head;
+
+        while (copyHead!=null) {
+            System.out.println(copyHead.data+" ");
+            copyHead=copyHead.next;
+        }
 
     }
 }
