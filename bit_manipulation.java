63de35b1f6ef2c92d11e6795f57d2af374fa3ef0
Piyush Sethiya <piyushseth226@gmail.com>
2024-08-01 23:59:29 +0530

    bits-half

---
+++ bit_manipulation.java
@@ -0,0 +1,23 @@
+public class bit_manipulation {
+    public static void main(String[] args) {
+        
+    // Get Bit Operation.
+        int n = 5;
+        int pos = 1;
+        int bitMask = 1<<pos;
+        // if((bitMask & n) == 0 ){
+        //     System.out.println("bit is Zero.");
+        // } else {
+        //     System.out.println("Bit is One");
+        // }
+    
+    // Set Bit Operation
+
+        // int set_res = bitMask | n;
+        // System.out.println(set_res);
+
+    // Clear Bit Operation
+
+        
+    }
+}
