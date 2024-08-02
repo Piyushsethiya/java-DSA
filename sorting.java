3ee2aa14f4195eaea0f8659e0972ee5a6965a6f7
Piyush Sethiya <piyushseth226@gmail.com>
2024-08-02 16:39:44 +0530

    selection done

--- sorting.java
+++ sorting.java
@@ -8,7 +8,7 @@ public class sorting {
     public static void main(String[] args) {
         int arr[] = {7,8,3,1,2};
 
-        //bubble sort 
+    //bubble sort 
         for(int i = 0; i<arr.length-1; i++){
             for(int j = 0; j<arr.length-1-i; j++){
                 if(arr[j]> arr[j+1]){
@@ -21,12 +21,23 @@ public class sorting {
         }
         // printArray(arr);
 
-        // Selection Sort
+    // Selection Sort
         for(int i = 0; i<arr.length-1;i++){
-            for(int j = 0; j<arr.length-1-i; j++){
+            int smallest = i;
+            for(int j = i+1; j<arr.length; j++){
                 //arr[i] is smallest
-                // if(arr[i])
+                if(arr[smallest]> arr[j] ){
+                    smallest = j;
+                }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
+        // printArray(arr);
+
+    //insertion Sort 
+
+        
     }
 }
